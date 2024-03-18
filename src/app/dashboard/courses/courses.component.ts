95ab3e4f25fbd33ed5175a8f1cf20c2f52fa5ce1
quanlta <lamthanhanhquan115@gmail.com>
2024-03-18 11:56:51 +0700

    admin courses

--- src/app/dashboard/courses/courses.component.ts
+++ src/app/dashboard/courses/courses.component.ts
@@ -5,8 +5,16 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { DataTableDirective, DataTablesModule } from 'angular-datatables';
 import { Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CoursesService } from '../../api/services/courses/courses.service';
@@ -14,38 +22,58 @@ import { CoursesService } from '../../api/services/courses/courses.service';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [FormsModule, NgxPaginationModule],
+  imports: [FormsModule, CommonModule, NgxPaginationModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css',
 })
 export class CoursesComponent {
-  coursesList: any;
+  coursesList: any[] = [];
+  filteredCourses: any[] = [];
   searchTerm: string = '';
   currentPage: number = 1;
   itemsPerPage: number = 5;
-  constructor(private coursesService: CoursesService) { }
+  constructor(    
+    private router: Router, 
+    private route:ActivatedRoute,
+    private coursesService: CoursesService) { 
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    }
   ngOnInit() {
-    this.getListCourses()
+    this.searchTerm = this.route.snapshot.params['searchTerm'];
+    // this.getListCourses()
+    this.getCourses();
   }
-  filterCourses(): any[] {
-    return this.coursesList.filter((course: any) =>
+  chunkArray(array: any[], size: number): any[][] {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i += size) {
+      chunkedArray.push(array.slice(i, i + size));
+    }
+    return chunkedArray;
+  }
+  filterCourses(): void {
+    this.filteredCourses = this.coursesList.filter((course: any) =>
       course.name.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
   }
-
-  getListCourses() {
-    this.coursesService.getAll().subscribe(
-      (response) => {
-        console.log('response', response);
-
-        alert('delete user success');
-      },
-      (error) => {
-        // Handle login error
-        console.error('delete user failed', error);
-      }
-    );
+  trackByIdx(index: number, item: any): number {
+    return item.courseID;
   }
+  getCourses(): void {
+    this.coursesService.getAllCourses()
+      .subscribe(courses => {
+        this.coursesList = courses;
+      });
+  }
+  // getListCourses(){
+  //   this.coursesService.findByTitle(this.searchTerm).subscribe({
+  //     next:res =>{
+  //       this.coursesList = res;
+  //       console.log(this.coursesList);
+  //     },error: err =>{
+  //       console.log(err);
+  //     }
+  //   })
+  // }
   showAddCourseForm() {
     // Show the add course modal
     const addCourseModal = document.getElementById('addCourseModal');
